ee655f890fefdb720a1c072a9c56a2060c598234
HP <HP@192.168.219.104>
2021-12-12 21:30:23 +0900

    검색버튼 클릭시 검색 결과 보여줌

--- src/Search/SearchDisplay.java
+++ src/Search/SearchDisplay.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
 import java.awt.event.*;
+import DB.*;
+import javax.swing.border.LineBorder;
 
 /**
  * 검색 화면을 구성한 클래스
@@ -27,7 +29,6 @@ public class SearchDisplay extends JFrame {
 		Txt_input_search.setBounds(88, 23, 226, 38);
 		getContentPane().add(Txt_input_search);
 		Txt_input_search.setColumns(30);
-		String txt = Txt_input_search.getText();
 		
 		/**
 		 * 검색 이미지가 추가된 버튼
@@ -38,8 +39,39 @@ public class SearchDisplay extends JFrame {
 		getContentPane().add(Btn_search);
 		
 		
+		/**
+		 * 검색 버튼 클릭시 검색 처리
+		 */
+		Btn_search.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String txt = Txt_input_search.getText();
+				
+				SearchProcess obj = new SearchProcess();
+				String temp [];
+				
+				if(obj.IsEmpty(txt)) {
+					temp = obj.search_res(txt);
+					
+					JPanel panel = new JPanel();
+					panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+					panel.setBounds(-23, 102, 400, 100);
+					getContentPane().add(panel);
+					panel.setLayout(null);
+					
+					JButton Btn_foodtruck = new JButton(temp[0]);
+					Btn_foodtruck.setBounds(45, 10, 301, 80);
+					panel.add(Btn_foodtruck);
+					
+				}
+				else {
+					JOptionPane errorlog = new JOptionPane();
+					JOptionPane.showMessageDialog(null, "검색한 결과가 없습니다.", "검색 오류", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			
+		});
+		
 	}
-	
-	
-	
 }
