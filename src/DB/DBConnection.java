9468c7f59de8746e9465a87d3f38ff6e05f36796
HP <HP@DESKTOP-KV2AJ4Q>
2021-12-16 14:30:03 +0900

    DB연결 코드 수정

--- src/DB/DBConnection.java
+++ src/DB/DBConnection.java
@@ -18,22 +18,25 @@ public class DBConnection {
 	Connection conn = null;
 	Statement stmt = null; 
 	
-	public Statement db_connect() {
-		
+	public  Statement db_connect() {
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver"); 
+			// 연결 생성
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection(jdbc_url, db_id, db_password); 
-			System.out.println( "DB연결완료. " );	
-			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE); 
-			
-		} catch( ClassNotFoundException e ) {
-			System.out.println( "해당드라이버를찾을수없습니다.\n" + e);
-		} catch( SQLException e) {
-			System.out.println( "해당드라이버를찾을수없습니다.\n" + e);
+			System.out.println("DB연결 성공");
+			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);   
+			// 연결로부터 statement 생성
+		} 
+		catch (SQLException ex) 
+		{
+			System.out.println("DB연결 오류");
+		} 
+		catch (ClassNotFoundException ex) 
+		{
+			System.out.println("JDBC 드라이버 로드 오류 "); 
 		}
-		
 		return stmt;
-	}
+	}  
 	
 	/**
 	* 데이터베이스와의 연결 해제
