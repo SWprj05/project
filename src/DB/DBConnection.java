9eb8964b0bda35c39126ea223707b1d11d664e4c
HP <HP@DESKTOP-KV2AJ4Q>
2021-12-16 12:52:44 +0900

    DB연결 코드 수정

--- src/DB/DBConnection.java
+++ src/DB/DBConnection.java
@@ -3,7 +3,7 @@ package DB;
 import java.io.*;
 import java.lang.*;
 import java.sql.*;
-
+import java.util.logging.*;
 
 /**
  * 데이터베이스에 연결합니다.
@@ -24,8 +24,7 @@ public class DBConnection {
 			Class.forName("oracle.jdbc.driver.OracleDriver"); 
 			conn = DriverManager.getConnection(jdbc_url, db_id, db_password); 
 			System.out.println( "DB연결완료. " );	
-			stmt = conn.createStatement(); 
-			
+			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE); 
 			
 		} catch( ClassNotFoundException e ) {
 			System.out.println( "해당드라이버를찾을수없습니다.\n" + e);
@@ -35,6 +34,19 @@ public class DBConnection {
 		
 		return stmt;
 	}
+	
+	/**
+	* 데이터베이스와의 연결 해제
+	*/
 
+	public  void db_close() { 
+		try 
+		{
+			conn.close();
+		} 
+		catch (SQLException ex) {
+			Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex); 
+		}
+	}
 
 }
