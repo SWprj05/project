1a4198add79620c8fd292f9dc0accd1eace9ce47
HP <HP@DESKTOP-KV2AJ4Q>
2021-12-16 14:34:32 +0900

    검색 결과 배열이 비어있는지 체크하는 함수 삭제

--- src/Search/SearchProcess.java
+++ src/Search/SearchProcess.java
@@ -12,31 +12,12 @@ public class SearchProcess {
 	FoodtruckDB obj = new FoodtruckDB();
 	
 	/**
-	 * 검색 결과가 비어있나 체크하는 메소드입니다.
-	 * @param txt 검색할 가게이름
-	 * @return true||false
-	 */
-	public boolean IsEmpty(String txt) {
-		String temp [] = obj.get_shop_name(txt);
-	
-		if(!temp[0].equals("1")) {
-			return true;
-		}
-		else if(temp[0].equals("1")){
-			return false;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	/**
 	 * 검색어로 검색해 가게 이름을 리턴해주는 메소드입니다. 
 	 * @param txt 검색할 가게이름
 	 * @return 검색결과 배열
 	 */
 	public String[] search_res(String txt) {
-		String arr [] = obj.get_shop_name(txt);
+		String arr[] = obj.get_search_res(txt);
 		
 		return arr;
 	}
